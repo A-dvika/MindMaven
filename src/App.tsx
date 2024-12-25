d1600b8d6e95509ea0c68ac25b7567162a653406
Advika Thakur <adici2403@gmail.com>
2024-12-25 20:40:07 +0530

    gitignores

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,9 @@ import ReactFlow, {
 import 'react-flow-renderer/dist/style.css';
 import 'react-flow-renderer/dist/theme-default.css';
 import { GoogleGenerativeAI } from '@google/generative-ai';
+const apiKey = import.meta.env.VITE_APP_GEMINI_API_KEY;
 
-const genAI = new GoogleGenerativeAI('AIzaSyAKAQnh-FBryXOcKjk80fiqouU6XMrXJQ8');
+const genAI = new GoogleGenerativeAI(apiKey);
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 const levelColors = [
